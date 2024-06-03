5ebc81b38ceb1f59d03a37d43ab18b63415e96fa
cphong223 <24A4042605@hvnh.edu.vn>
2024-06-03 16:29:45 +0700

    Form thêm hàng hóa

---
+++ QuanLyCuaHangMayTinh/QuanLyCuaHangMayTinh/Views/ThemHangHoa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyCuaHangMayTinh.Views
+{
+    public partial class ThemHangHoa : Form
+    {
+        public ThemHangHoa()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBoQua_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Bạn có muốn hủy bỏ việc thêm không?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (confirmResult == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
